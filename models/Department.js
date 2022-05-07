0b96261a497b63d43df008a339a9e63d5738422c
llattanzi <lauren71442@gmail.com>
2022-05-07 12:34:42 -0400

    create department model

---
+++ models/Department.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Department extends Model {
+
+}
+
+Department.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'department'
+    }
+);
+
+module.exports = Department;
