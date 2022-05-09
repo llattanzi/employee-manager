8a12ba39598d68e3aa78b9c05668095951aa6ca6
llattanzi <lauren71442@gmail.com>
2022-05-08 20:25:22 -0400

    add employee prompt and api post request

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function fetchRole(type) {
         }
         // create a list of role names to use as choices in prompt
         roleNames = response.map(function(elem) {
-            return elem.name;
+            return elem.title;
         });
         roleData = response;
 
@@ -181,27 +181,135 @@ function fetchEmployees(type) {
     .then(response => response.json())
     .then(response => {
         // create a list of employee names to use as choices in prompt for manager
-        // if no employees, set employeeNames to null
+        // if no employees, set employeeNames to None
         if (!response) {
-            employeeNames = "";
+            employeeNames = "None";
             return
         }
         employeeNames = response.map(function(elem) {
-            return elem.name;
+            return `${elem.first_name} ${elem.last_name}`;
         });
+        employeeNames.push('None');
+        console.log(employeeNames);
         employeeData = response;
 
         if (type === 'Add employee') {
             addEmployee();
         }
-        else if (type === 'Update employee') {
-            updateEmployee();
+        else {
+            updateEmployee(type);
         }
     })
 };
 
 function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "Enter the employee's first name"
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "Enter the employee's last name"
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Choose a role for the employee',
+            choices: roleNames
+        },
+        {
+            type: 'list',
+            name: 'department',
+            message: 'Choose a department that the role belongs to',
+            choices: deptNames
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: 'Choose a manager for the employee or select "None"',
+            choices: employeeNames
+        }
+    ])
+    .then(({ first_name, last_name, role, department, manager }) => {
+        // find the department id that corresponds to the selected dept name
+        let deptId;
+        for (i = 0; i < deptData.length; i++) {
+            if (deptData[i].name === department) {
+                deptId = deptData[i].id;
+                break;
+            }
+        }
+        // find the role id that corresponds to the selected role name
+        let roleId;
+        for (i = 0; i < roleData.length; i++) {
+            if (roleData[i].title === role) {
+                roleId = roleData[i].id;
+                break;
+            }
+        }
+        // find the manager id that corresponds to the selected manager name
+        let managerId;
+        // set manager ID to null if no manager chosen
+        if (manager === 'None') {
+            managerId = null;
+        }
+        else {
+            for (i = 0; i < employeeData.length; i++) {
+                let fullName = `${employeeData[i].first_name} ${employeeData[i].last_name}`;
+                if (fullName === manager) {
+                    managerId = employeeData[i].id;
+                    break;
+                }
+            }
+        }
+        // create a employee object to pass into post request body
+        const newEmployee = {
+            first_name: first_name,
+            last_name: last_name,
+            role_id: roleId,
+            manager_id: managerId,
+            department_id: deptId
+        };
 
+        // add new employee to database
+        fetch('http://localhost:3001/api/employee', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newEmployee)
+        })
+        .then(() => {
+            console.log(`Added ${first_name} ${last_name} to the database`)
+        })
+        .then(() => promptUser())
+    })
+};
+
+function updateEmployee(type) {
+    
+}
+
+function updatePrompt() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'attribute',
+        message: 'What would you like to update?',
+        choices: ['Role', 'Manager']
+    })
+    .then(({ attribute }) => {
+        if (attribute === 'Role') {
+            let type = 'Update role';
+            fetchRole(type);
+        }
+        else if (attribute === 'Manager') {
+            let type = 'Update manager';
+            fetchEmployees(type);
+        }
+    })
 }
 
 function promptUser() {
@@ -235,6 +343,9 @@ function promptUser() {
         else if (action === 'Add an employee') {
             let type = 'Add employee';
             fetchDept(type);
+        }
+        else if (action === 'Update an employee') {
+            updatePrompt();
         }
     })
 }
